9029b8c9faf94e1ed3e6005d00ed98a1c95b30e4
Arthur Silvino da Silva <arthursilvino902@gmail.com>
2024-10-31 11:20:23 -0300

    adicionando um metodo para acessar os detalhes de um episodio pelo id

--- src/routes/episodiosRoutes.ts
+++ src/routes/episodiosRoutes.ts
@@ -4,6 +4,20 @@ import db from "../db/knexfile";
 
 const router = express.Router();
 
+// ROTA PARA ACESSAR OS DETALHES DE UM EPISÓDIO PELO ID
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const episodio = await db("episodios").where({ id }).first();
 
+    if (!episodio) {
+       res.status(404).json({ error: "Episódio não encontrado." });
+    }
+
+    res.status(200).json(episodio);
+  } catch (error: any) {
+    res.status(500).json({ error: error.sqlMessage || error.message });
+  }
+});
 
 export default router;
