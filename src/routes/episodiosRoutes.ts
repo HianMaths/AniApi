1bd33950a912d43fc03a4a58511b01b5bd4a9b80
Yuque7 <yuque7@gmail.com>
2024-10-31 16:00:33 -0300

    Adicionando método para buscar episódio por ID

--- src/routes/episodiosRoutes.ts
+++ src/routes/episodiosRoutes.ts
@@ -4,34 +4,37 @@ import db from "../db/knexfile";
 
 const router = express.Router();
 
-router.post('/:animeId', async (req: Request, res: Response) => {
+router.post("/:animeId", async (req: Request, res: Response) => {
   const { animeId } = req.params;
-  const { titulo, descricao, numero } = req.body; 
+  const { titulo, descricao, numero } = req.body;
 
   try {
-    
-    const animeExists = await db('animes').where({ id: animeId }).first();
+    const animeExists = await db("animes").where({ id: animeId }).first();
 
     if (!animeExists) {
-      res.status(404).json({ message: 'Anime não encontrado.' });
+      res.status(404).json({ message: "Anime não encontrado." });
     }
 
     const id = uuidv7();
-    const novoEpisodio = await db('episodios').insert({
-      id,
-      anime_id: animeId, 
-      titulo,
-      descricao,
-      numero,
-    }).returning('*'); 
+    const novoEpisodio = await db("episodios")
+      .insert({
+        id,
+        anime_id: animeId,
+        titulo,
+        descricao,
+        numero,
+      })
+      .returning("*");
 
     res.status(201).json({
-      message: 'Episódio adicionado com sucesso.',
-      episodio: novoEpisodio[0], 
+      message: "Episódio adicionado com sucesso.",
+      episodio: novoEpisodio[0],
     });
   } catch (error: any) {
     console.error("Erro ao adicionar episódio:", error);
-    res.status(500).json({ message: error.message || "Erro interno do servidor." });
+    res
+      .status(500)
+      .json({ message: error.message || "Erro interno do servidor." });
   }
 });
 
@@ -77,6 +80,29 @@ router.put("/:id", async (req: Request, res: Response) => {
     res.status(200).json(updatedEpisodio[0]);
   } catch (error: any) {
     res.status(500).json({ error: error.sqlMessage || error.message });
+  }
+});
+
+// Método para buscar episódios
+router.get("/episodio/:episodioId", async (req: Request, res: Response) => {
+  try {
+    const { episodioId } = req.params;
+
+    const episodioExists = await db("episodios")
+      .where({ id: episodioId })
+      .first();
+    if (!episodioExists) {
+      res.status(404).json({ message: "Episódio não encontrado." });
+    }
+
+    const episodios = await db("episodios").where({ episodio_id: episodioId });
+
+    res.status(200).json(episodios);
+  } catch (error: any) {
+    console.error("Erro ao buscar episódios:", error);
+    res
+      .status(500)
+      .json({ message: error.message || "Erro interno do servidor." });
   }
 });
 
