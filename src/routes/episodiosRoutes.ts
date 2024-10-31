f8baf314e41e31b28fab432040fc20e1c257077e
Arthur Silvino da Silva <arthursilvino902@gmail.com>
2024-10-31 11:26:54 -0300

    adicionando um metodo para atualizar um episodio existente

--- src/routes/episodiosRoutes.ts
+++ src/routes/episodiosRoutes.ts
@@ -20,4 +20,37 @@ router.get("/:id", async (req: Request, res: Response) => {
   }
 });
 
+// ROTA PARA ATUALIZAR UM EPISÓDIO EXISTENTE PELO ID
+router.put("/:id", async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const {
+        titulo,
+        numero,
+        descricao,
+      } = req.body;
+  
+      if (!titulo && !numero && !descricao) {
+         res.status(400).json({ error: "Nenhum campo para atualização foi fornecido." });
+      }
+  
+      const updatedEpisodio = await db("episodios")
+        .where({ id })
+        .update({
+          titulo,
+          numero,
+          descricao,
+        })
+        .returning("*");
+  
+      if (updatedEpisodio.length === 0) {
+         res.status(404).json({ error: "Episódio não encontrado." });
+      }
+  
+      res.status(200).json(updatedEpisodio[0]);
+    } catch (error: any) {
+      res.status(500).json({ error: error.sqlMessage || error.message });
+    }
+  });
+  
 export default router;
