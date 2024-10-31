fbd27c0b7510c4113c92e0516d6c8bd9659cf71a
HianMaths <hianmatheusofc1@gmail.com>
2024-10-31 12:48:19 -0300

    Adição do endpoint do Método POST (rota que adiciona um episódio a um anime específico) da rota Episódios na Api.

--- build/routes/episodiosRoutes.js
+++ build/routes/episodiosRoutes.js
@@ -13,8 +13,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const uuid_1 = require("uuid");
 const knexfile_1 = __importDefault(require("../db/knexfile"));
 const router = express_1.default.Router();
+router.post('/:animeId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { animeId } = req.params;
+    const { titulo, descricao, numero } = req.body;
+    try {
+        const animeExists = yield (0, knexfile_1.default)('animes').where({ id: animeId }).first();
+        if (!animeExists) {
+            res.status(404).json({ message: 'Anime não encontrado.' });
+        }
+        const id = (0, uuid_1.v7)();
+        const novoEpisodio = yield (0, knexfile_1.default)('episodios').insert({
+            id,
+            anime_id: animeId,
+            titulo,
+            descricao,
+            numero,
+        }).returning('*');
+        res.status(201).json({
+            message: 'Episódio adicionado com sucesso.',
+            episodio: novoEpisodio[0],
+        });
+    }
+    catch (error) {
+        console.error("Erro ao adicionar episódio:", error);
+        res.status(500).json({ message: error.message || "Erro interno do servidor." });
+    }
+}));
 router.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { id } = req.params;
@@ -31,9 +58,11 @@ router.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () 
 router.put("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { id } = req.params;
-        const { titulo, numero, descricao, } = req.body;
+        const { titulo, numero, descricao } = req.body;
         if (!titulo && !numero && !descricao) {
-            res.status(400).json({ error: "Nenhum campo para atualização foi fornecido." });
+            res
+                .status(400)
+                .json({ error: "Nenhum campo para atualização foi fornecido." });
         }
         const updatedEpisodio = yield (0, knexfile_1.default)("episodios")
             .where({ id })
