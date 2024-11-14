9a9d2b2dc502ca43f10121fefa2b10cfb99ad0ab
Yuque7 <yuque7@gmail.com>
2024-11-14 13:48:25 -0300

    Adicionando rota para buscar personagem de um anime

--- src/routes/personagensRoutes.ts
+++ src/routes/personagensRoutes.ts
@@ -19,4 +19,20 @@ router.get("/:id", async (req: Request, res: Response) => {
   }
 });
 
+// Rota para buscar personagem de um anime
+router.get("/:animeId/personagens", async (req, res) => {
+  try {
+    const { animeId } = req.params;
+    const personagens = await db("personagens").where({ anime_id: animeId });
+    if (personagens.length === 0) {
+      res.status(404);
+      throw new Error("Nenhum personagem encontrado para este anime.");
+    }
+    res.status(200).json(personagens);
+  } catch (error: any) {
+    const message = error.sqlMessage || error.message;
+    res.json(message);
+  }
+});
+
 export default router;
